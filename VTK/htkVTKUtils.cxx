96261b09759b8e5deba3cc080f16fa3031e5ea5d
Tristan Coulange <tristan.coulange@kitware.com>
2019-07-25 14:54:56 +0200

    Setup VTK mechanism

--- VTK/htkVTKUtils.cxx
+++ VTK/htkVTKUtils.cxx
@@ -1,4 +1,37 @@
+#include <vtkActor.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSphereSource.h>
+
+
 namespace htk
 {
-	
+  static void CreateSphereSource()
+  {
+    vtkNew<vtkSphereSource> sphereSource;
+    sphereSource->SetCenter(0.0, 0.0, 0.0);
+    sphereSource->SetRadius(5.0);
+
+    vtkNew<vtkPolyDataMapper> mapper;
+    mapper->SetInputConnection(sphereSource->GetOutputPort());
+
+    vtkNew<vtkActor> actor;
+    actor->SetMapper(mapper.Get());
+
+    vtkNew<vtkRenderer> renderer;
+    renderer->AddActor(actor.Get());
+    renderer->SetBackground(.3, .6, .3);
+
+    vtkNew<vtkRenderWindow> renderWindow;
+    renderWindow->AddRenderer(renderer.Get());
+
+    vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+    renderWindowInteractor->SetRenderWindow(renderWindow.Get());
+
+    renderWindow->Render();
+    renderWindowInteractor->Start();
+  }
 }
