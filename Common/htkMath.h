5cece8f13660d9fb890d7f4e1329e24cf9c2e5bb
unknown <laurenn.lam@kitware.com>
2019-09-19 16:05:18 +0200

    STYLE: Replace ifndef by pragma once and add new namespace to Math class

--- Common/htkMath.h
+++ Common/htkMath.h
@@ -1,13 +1,12 @@
-#ifndef htkcommonutils_h
-#define htkcommonutils_h
+#pragma once
 
 #include "htkcommonlib_export.h"
 
 namespace htk
 {
-
-HTKCOMMONLIB_EXPORT void vectorAB(const double A[3], const double B[3], double AB[3]);
-
+	namespace math
+	{
+		HTKCOMMONLIB_EXPORT void vectorAB(const double A[3], const double B[3], double AB[3]);
+	}
 }
 
-#endif
