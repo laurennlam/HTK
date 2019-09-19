f7144816d9a1b96366c6253dcb3c62a9520ae86e
unknown <laurenn.lam@kitware.com>
2019-09-19 16:04:44 +0200

    FEAT: Add IO class in Common lib

---
+++ Common/htkIO.cxx
@@ -0,0 +1,20 @@
+#include "htkIO.h"
+
+#include <iostream>
+#include <fstream>
+
+void htk::io::writeOutputFile(const std::string& filePath, const std::string& content,
+	const bool appendContent)
+{
+	std::ofstream myfile;
+	if (appendContent)
+	{
+		myfile.open(filePath, std::ios::app);
+	}
+	else
+	{
+		myfile.open(filePath);
+	}
+	myfile << content.c_str();
+	myfile.close();
+}
